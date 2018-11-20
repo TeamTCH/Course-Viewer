d8b4141fa74b11674669621f430b4aaa4f5b75ba
Kevin Lopez <link4912@gmail.com>
2018-11-20 17:24:49 -0500

    Found a way to schedule

--- data-scraping/index.js
+++ data-scraping/index.js
@@ -15,6 +15,8 @@ function run() {
 
       //go to page and wait till the network is idle
       await page.goto(url, { waitUntil: 'networkidle2' });
+      //lets us use findElementByText script
+
       //fill in username and password
       await page.waitFor('input[name=userid]');
       await page.$eval('input[name=userid]', el => el.value = 'LOPEZKEV');
@@ -22,11 +24,11 @@ function run() {
       await page.waitFor('input[name=pwd]');
       await page.$eval('input[name=pwd]', el => el.value = 'iWTXeerB');
 
-      await page.screenshot({ path: 'screenshots/inputEntered.png', fullPage: true });
+      await page.screenshot({ path: 'data-scraping/screenshots/1inputEntered.png', fullPage: true });
 
       //submit login request
       await page.click('input[type="submit"]');
-      await page.screenshot({ path: 'screenshots/loggedIn.png', fullPage: true });
+      await page.screenshot({ path: 'data-scraping/screenshots/2loggedIn.png', fullPage: true });
 
       await page.waitFor(5000) // we need to wait for Twitter widget to load
 
@@ -34,19 +36,16 @@ function run() {
       for (const frame of page.mainFrame().childFrames()) {
         // Here you can use few identifying methods like url(),name(),title()
         if (frame.url().includes('sheridan')) {
-          await page.screenshot({ path: 'screenshots/frameLoaded.png', fullPage: true });
+          await page.screenshot({ path: 'data-scraping/screenshots/3frameLoaded.png', fullPage: true });
           console.log('we found the sheridan iframe')
           scheduleFrame = frame
         }
       }
 
-      //await clickByText(page,'Weekly Schedule')
-      //scrape all contents on the page that's in a table, including the html
-      const data = await page.evaluate(() => {
-        const tds = Array.from(document.querySelectorAll('table tr td'))
-        return tds.map(td => td.innerHTML)
-      });
-      console.log(data);
+      //now go to the schedule frame
+      const scheduleUrl = 'https://psoft-sa-students.sheridancollege.ca/psc/saprodlb/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_SCHD_W.GBL?Page=SSR_SS_WEEK&Action=A&ExactKeys=Y&EMPLID=991395035&TargetFrameName=None&PortalActualURL=https%3a%2f%2fpsoft-sa-students.sheridancollege.ca%2fpsc%2fsaprodlb%2fEMPLOYEE%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSR_SSENRL_SCHD_W.GBL%3fPage%3dSSR_SS_WEEK%26Action%3dA%26ExactKeys%3dY%26EMPLID%3d991395035%26TargetFrameName%3dNone&PortalContentURL=https%3a%2f%2fpsoft-sa-students.sheridancollege.ca%2fpsc%2fsaprodlb%2fEMPLOYEE%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSR_SSENRL_SCHD_W.GBL&PortalContentProvider=HRMS&PortalCRefLabel=My%20Weekly%20Schedule&PortalRegistryName=EMPLOYEE&PortalServletURI=https%3a%2f%2fpsoft-sa-students.sheridancollege.ca%2fpsp%2fsaprodlb%2f&PortalURI=https%3a%2f%2fpsoft-sa-students.sheridancollege.ca%2fpsc%2fsaprodlb%2f&PortalHostNode=HRMS&NoCrumbs=yes&PortalKeyStruct=yes'
+      await page.goto(scheduleUrl,{waitUntil: 'networkidle2'})
+      await page.screenshot({ path: 'data-scraping/screenshots/4schedulePage.png', fullPage: true });
 
       browser.close();
       return resolve('Done!');
