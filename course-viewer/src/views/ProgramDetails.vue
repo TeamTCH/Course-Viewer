0bce625f6873ed0fb59aeb5ea392630497f232e3
Calvin Cheung <calvincheung@Calvins-MacBook-Pro.local>
2018-11-25 17:57:45 -0500

    Began making ProgramDetails.vue

---
+++ course-viewer/src/views/ProgramDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import Courses from '../assets/data/Courses.json'
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
